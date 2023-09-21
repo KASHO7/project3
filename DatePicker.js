eeffbf82c5b98960b0e5a4744ac3ad78c44a7ca6
KASHO7 <96492831+KASHO7@users.noreply.github.com>
2023-09-21 15:35:53 -0400

    worked on datepicker.js

--- DatePicker.js
+++ DatePicker.js
@@ -1,14 +1,28 @@
 'use strict';
 class DatePicker{
     constructor(id, callBack) {
-    this.id = id;
-    this.callBack = callBack;
-    this.date = new Date();
+        this.id = id;
+        this.callBack = callBack;
+        this.date = new Date();
     }
     render(date){
         const div = document.getElementById(this.id);
-        if(!div){
-            console.error(`No element with id ${this.id} found`);
-        } 
+        this.date = date;
+        const month = this.date.getMonth();
+        const year = this.date.getFullYear();
+        const day = this.date.getDate();
+        const lastDay = new Date(year, month + 1, 0);
+        const fistDay = new Date(year, month, 1);
+        const daysInMonth = lastDay.getDate();
+        const fistDayOfWeek = fistDay.getDay();
+
+    
+        div.innerHTML = '';
+
     }
+    getMonthName(month){
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septemper', 'October', 'November', 'December'];
+        return months[month];
+    }
+    
 }
